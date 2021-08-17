a6b5800fe98d5b0ee3a89429baff1608fbe84d44
Lorenzo <87342158+Lbriozzo95@users.noreply.github.com>
2021-08-17 18:45:34 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+
+import  express from 'express'
+
+
+let productos = []
+
+class Producto {
+    constructor (title, price, thumbnail) {
+        this.id = productos.length+1
+        this.title = title
+        this.price = price
+        this.thumbnail = thumbnail
+    }
+}
+
+const port = 8080
+const app = express()
+
+const server = app.listen(port, () => {
+    console.log("Servidor en linea en el puerto " + server.address().port)
+})
+
+productos.push(new Producto ("Buzos", 4800, "/foto4.jpg"))
+productos.push(new Producto ("Camperas", 5700, "/foto5.jpg"))
+
+
+
+app.get("/api/productos/listar", (req, res) => {
+    try{
+        if(productos.length == 0){
+            res.status(404).json({"error": "No se encontaron productos"})
+        }else {
+            res.status(200).json(productos)
+        }
+    }catch(err) {
+        res.status(404).json({err})
+    }
+
+})
+
+
+app.get("/api/productos/listar/:id", (req, res) => {
+    try{
+        if (req.params.id <= (productos.length)) {
+            res.status(200).json(productos[req.params.id-1])
+        } else {
+            res.status(404).json({"error": "No se encontaron productos"})
+        }
+    }catch(err) {
+        res.status(404).json({err})
+    }
+})
+
+app.post("/api/productos/guardar/", (req, res) => {
+   
+    let title = req.query.title
+    let price = parseInt(req.query.price)
+    let thumbnail = req.query.price
+  
+
+    try{
+            productos.push(new Producto(title, price, thumbnail))
+            res.status(200).json(productos[productos.length -1])
+        
+    }catch(err) {
+        res.status(404).json(err)
+    }
+})
